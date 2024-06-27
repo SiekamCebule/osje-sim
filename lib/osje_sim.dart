c3fda362cb56b49ba8aa0f9ed5fb01a6285925f3
Konrad <konradkrol110@gmail.com>
2024-06-27 12:54:58 +0200

    Update barrel file

--- lib/osje_sim.dart
+++ lib/osje_sim.dart
@@ -1,1 +1,19 @@
 library;
+
+export 'src/enums/jumps_consistency.dart';
+export 'src/enums/landing_safety.dart';
+export 'src/enums/landing_style.dart';
+export 'src/enums/landing_type.dart';
+export 'src/enums/wind_general_direction.dart';
+
+export 'src/models/jump_simulation_record.dart';
+export 'src/models/simulation_parameters/jumper_simulation_parameters.dart';
+export 'src/models/simulation_parameters/hill_simulation_parameters.dart';
+export 'src/models/simulation_parameters/technical_simulation_parameters.dart';
+export 'src/models/simulation_parameters/weather_simulation_parameters.dart';
+export 'src/models/wind/degrees.dart';
+export 'src/models/wind/wind.dart';
+export 'src/models/wind/wind_during_jump.dart';
+
+export 'src/simulator/jump_simulator.dart';
+export 'src/simulator/osje/osje_jump_simulator.dart';
