f2714b76152614add35746c775f9dd9d83ba2312
Konrad <konradkrol110@gmail.com>
2024-07-09 14:02:56 +0200

    Add JSON support for Degrees type

--- lib/src/models/wind/degrees.dart
+++ lib/src/models/wind/degrees.dart
@@ -4,4 +4,10 @@ extension type Degrees(int value) {
   bool get isValid => value >= 0 && value <= 360;
   Degrees get remainder => Degrees(value % 360);
   double toRadians() => 2 * (value / 360) * pi;
+
+  static Degrees fromJson(String json) {
+    return Degrees(int.parse(json));
+  }
+
+  String toJson() => value.toString();
 }
