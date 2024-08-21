fef5fa6cae676651f5d57526b128095014404515
Konrad <konradkrol110@gmail.com>
2024-08-21 13:58:28 +0200

    Remove the weights from WindMeasurement class

--- lib/src/models/wind/wind_measurement.dart
+++ lib/src/models/wind/wind_measurement.dart
@@ -5,28 +5,15 @@ import 'package:osje_sim/src/models/wind/wind.dart';
 class WindMeasurement {
   const WindMeasurement({
     required this.winds,
-    required this.weights,
   });
 
   final Map<(double, double), Wind> winds;
-  final List<double> weights;
 
   Wind windByDistance(double distance) {
     final appropriateRange = winds.keys.singleWhere((range) {
       return distance >= range.$1 && distance <= range.$2;
     });
     return winds[appropriateRange]!;
-  }
-
-  double weightByDistance(double distance) {
-    int index = 0;
-    for (var range in winds.keys) {
-      final withinRange = distance >= range.$1 && distance <= range.$2;
-      if (withinRange) {
-        index = winds.keys.toList().indexOf(range);
-      }
-    }
-    return weights[index];
   }
 
   (double, double) rangeByIndex(int index) {
