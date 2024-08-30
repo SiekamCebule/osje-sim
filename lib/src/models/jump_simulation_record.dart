dcf9e2630275eef722ca03a0f2b5049f5187cdae
Konrad <konradkrol110@gmail.com>
2024-08-30 14:14:35 +0200

    Update jump simulation record class

--- lib/src/models/jump_simulation_record.dart
+++ lib/src/models/jump_simulation_record.dart
@@ -1,8 +1,9 @@
+import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:osje_sim/src/enums/landing_type.dart';
 
 @JsonSerializable()
-class JumpSimulationRecord {
+class JumpSimulationRecord with EquatableMixin {
   const JumpSimulationRecord({
     required this.distance,
     required this.landingType,
@@ -10,4 +11,10 @@ class JumpSimulationRecord {
 
   final double distance;
   final LandingType landingType;
+
+  @override
+  List<Object?> get props => [
+        distance,
+        landingType,
+      ];
 }
